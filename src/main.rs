2cb7b714bd4ff75adc6c9d39b13d91dfd8b504e9
Dmitry Suzdalev <mail@dimsuz.ru>
2023-05-19 01:31:47 +0200

    Add config parser crate
    Along with the test example

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,6 @@
 use anyhow::{Ok, Result};
 use clap::Parser;
+use configparser::ini::Ini;
 
 #[derive(Parser)]
 struct Args {
@@ -10,5 +11,13 @@ struct Args {
 fn main() -> Result<()> {
     let args = Args::parse();
     println!("{} {}", args.input_dir, args.output_dir);
+    println!();
+    ini_test();
     Ok(())
+}
+
+fn ini_test() {
+    let mut config = Ini::new();
+    let map = config.load("/tmp/strings.txt").unwrap();
+    println!("{:?}", map);
 }
