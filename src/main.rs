f52ac878f594348debe78cdaa152d10c9bde56c7
TrueWarg <grazashar@mail.ru>
2023-05-18 21:42:16 +0200

    Add test func test framework and basic test

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,14 @@
-fn main() {
-    println!("Hello, world!");
+use anyhow::{Ok, Result};
+use clap::Parser;
+
+#[derive(Parser)]
+struct Args {
+    input_dir: String,
+    output_dir: String,
+}
+
+fn main() -> Result<()> {
+    let args = Args::parse();
+    println!("{} {}", args.input_dir, args.output_dir);
+    Ok(())
 }
