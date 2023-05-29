4d5526b5b9344f067550cf1a4fd67c463b1986b2
TrueWarg <grazashar@mail.ru>
2023-05-29 23:32:16 +0200

    Write generated result in files

--- tests/test.rs
+++ tests/test.rs
@@ -4,15 +4,40 @@ use std::error::Error;
 
 #[test]
 fn case_android_1() -> Result<(), Box<dyn Error>> {
+    basic_test_case("case1")
+}
+
+#[test]
+fn case_android_2() -> Result<(), Box<dyn Error>> {
+    basic_test_case("case2")
+}
+
+#[test]
+fn case_android_3() -> Result<(), Box<dyn Error>> {
+    basic_test_case("case3")
+}
+
+#[test]
+fn case_android_4() -> Result<(), Box<dyn Error>> {
+    basic_test_case("case4")
+}
+
+#[test]
+fn case_android_5() -> Result<(), Box<dyn Error>> {
+    basic_test_case("case5")
+}
+
+fn basic_test_case(case_rel_path: &str) -> Result<(), Box<dyn Error>> {
     let mut cmd = Command::cargo_bin("utas")?;
 
     let temp = assert_fs::TempDir::new()?;
 
-    let input = "tests/cases/android/output1";
+    let input = format!("tests/cases/android/{}/input", case_rel_path);
     let output = temp.path();
+    let expected = format!("tests/cases/android/{}/output", case_rel_path);
 
-    cmd.arg(input).arg(output.as_os_str());
+    cmd.arg(&input).arg(output.as_os_str());
     cmd.assert().success();
-    assert!(file::dirs_contents_are_same(input, output)?);
+    assert!(file::dirs_contents_are_same(expected, output)?);
     Ok(())
 }
