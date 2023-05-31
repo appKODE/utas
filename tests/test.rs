cca7ce0e4fd0a831799a97125cb9c19d288f169a
Dmitry Suzdalev <mail@dimsuz.ru>
2023-06-01 00:57:31 +0200

    Fix string name generation bug
    Treat txt-file key names as case sensitive

--- tests/test.rs
+++ tests/test.rs
@@ -32,6 +32,11 @@ fn case_android_6() -> Result<(), Box<dyn Error>> {
     basic_test_case("case6")
 }
 
+#[test]
+fn case_android_7() -> Result<(), Box<dyn Error>> {
+    basic_test_case("case7")
+}
+
 fn basic_test_case(case_rel_path: &str) -> Result<(), Box<dyn Error>> {
     let mut cmd = Command::cargo_bin("utas")?;
 
