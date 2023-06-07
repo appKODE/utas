ce3b43d495e6886f115b554a4cff5b6dedd88155
TrueWarg <irrmm9000@gmail.com>
2023-06-07 18:52:34 +0200

    Print diffs on tests failures

--- tests/test.rs
+++ tests/test.rs
@@ -1,6 +1,7 @@
 use assert_cmd::Command;
 use assert_fs::{self};
-use std::error::Error;
+use file::{CompareDirsContentResult, Diff, DirDiff};
+use std::{error::Error, path::Path};
 
 #[test]
 fn case_android_1() -> Result<(), Box<dyn Error>> {
@@ -53,6 +54,63 @@ fn basic_test_case(case_rel_path: &str) -> Result<(), Box<dyn Error>> {
 
     cmd.arg(&input).arg(output.as_os_str());
     cmd.assert().success();
-    assert!(file::dirs_contents_are_same(expected, output)?);
+    let result = file::compare_dirs_content(expected, output)?;
+    match &result {
+        CompareDirsContentResult::Eq => (),
+        CompareDirsContentResult::Diffs(diffs) => eprintln!("{}", format_diffs(diffs)),
+    }
+    assert!(CompareDirsContentResult::Eq == result, "Dirs are different. See log above");
     Ok(())
+}
+
+fn format_result(result: CompareDirsContentResult) -> String {
+    match result {
+        CompareDirsContentResult::Eq => "OK".to_string(),
+        CompareDirsContentResult::Diffs(diffs) => format_diffs(&diffs),
+    }
+}
+
+fn format_diffs(diffs: &Vec<DirDiff>) -> String {
+    let mut result = "".to_string();
+    let mut index = 0;
+    for diff in diffs {
+        let item = match diff {
+            DirDiff::Path { left, right } => format!(
+                "{}. Paths are different: {} and {}\n___________________________________________________________\n\n",
+                index,
+                format_path(left),
+                format_path(right)
+            ),
+            DirDiff::FileContent { path, diffs } => {
+                format!(
+                    "{}. In file {} diff content:\n {}___________________________________________________________\n\n",
+                    index, 
+                    path,
+                    format_file_diffs(diffs),
+                )
+            }
+        };
+        index += 1;
+        result.push_str(&item);
+    }
+    result
+}
+
+fn format_path(path: &Option<String>) -> String {
+    match path {
+        Some(path) => path.clone(),
+        None => "|NO ANALOGUE|".to_string(),
+    }
+}
+
+fn format_file_diffs(diffs: &Vec<Diff>) -> String {
+    let mut result = "".to_string();
+    for diff in diffs {
+        result.push_str(&format!(
+            "  Line {}.:\n{}\n{}",
+            diff.line_number, diff.left, diff.right
+        ));
+    }
+
+    result
 }
