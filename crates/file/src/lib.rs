70d95b17fb0e7bf70fa8eceb4ef06834ee2f5d8e
TrueWarg <grazashar@mail.ru>
2023-06-08 22:25:30 +0200

    Run crate file test on ci

--- crates/file/src/lib.rs
+++ crates/file/src/lib.rs
@@ -1,3 +1,11 @@
-pub use file::*;
+pub use crate::file::compare_dirs_content;
+pub use crate::file::compare_files_content;
+pub use crate::file::copy_recursively;
+pub use crate::file::dirs_contents_are_same;
+pub use crate::file::files_are_same;
+pub use crate::file::CompareContentResult;
+pub use crate::file::CompareDirsContentResult;
+pub use crate::file::Diff;
+pub use crate::file::DirDiff;
 
 mod file;
